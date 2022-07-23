b4fcbb0766903d8cb7e9bf0ded47028b29463b9b
SahTitus <89194958+SahTitus@users.noreply.github.com>
2022-07-23 17:09:28 +0000

    commit

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
  const allowedOrigins = [
 	"http://127.0.0.1:5500",
 	"http://localhost:3000",
-	"https://hooklearn-dcd07.web.app"
+	"https://hooklearn-dcd07.web.app",
+	"https://hook-murex.vercel.app"
 ];
 
 export default allowedOrigins
